ef88b62a56b3e24761296a26e12525e750cfaa55
mbondoc <maxbondoc11@gmail.com>
2024-02-26 14:38:30 -0500

    uh huh

---
+++ circle.java
@@ -0,0 +1,18 @@
+/********************************
+ * Programmer: Max Bondoc
+ * Date: February 2024
+ * Program Name: circle.java
+ *********************************/
+
+public class circle{
+    public static void main(String[]args){
+        double radius = 4.28;
+        double area = 3.14*(radius*radius);
+        double circumference =  2*3.14*radius;
+
+        System.out.println("The area of a circle is "+area);
+        System.out.print("The circumference of a circle is "+circumference);
+        
+    }
+
+}
---
+++ rect.java
@@ -0,0 +1,12 @@
+/********************************
+ * Programmer: Max Bondoc
+ * Date: February 2024
+ * Program Name: rect.java
+ *********************************/
+
+public class rect{
+    public static void main(String[]args){
+        System.out.print("***************\n*\t      *\n*\t      *\n*\t      *\n*\t      *\n*\t      *\n***************");
+    }
+
+}
